bdc22e0d046683c5340f1c60e9c8f6e7621c5078
hgl2001 <hgl2019@sjtu.edu.cn>
2024-12-07 01:33:44 +0800

    On branch master Changes to be committed: 	new file:   app/chat/layout.tsx 	new file:   app/chat/page.tsx 	modified:   app/globals.css 	modified:   app/layout.tsx 	modified:   app/page.tsx 	new file:   components/common/Button.tsx 	new file:   components/home/Main.tsx 	new file:   components/home/Navigation/Menubar.tsx 	new file:   components/home/Navigation/index.tsx 	modified:   package-lock.json 	modified:   package.json 	modified:   tailwind.config.js

---
+++ components/home/Navigation/index.tsx
@@ -0,0 +1,9 @@
+import Menubar from "./Menubar";
+
+export default function Navigation() {
+  return (
+    <nav className="h-full w-[260px] bg-gray-900 text-gray-300 p-2">
+      <Menubar />
+    </nav>
+  );
+}
